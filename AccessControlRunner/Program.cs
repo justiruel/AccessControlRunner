b14a695bb9e66c25b072169b6bc329d1ac692e2f
tafsirul <tafsirulka@gmail.com>
2019-01-30 15:08:05 +0700

    .

---
+++ AccessControlRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AccessControlRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string strCmdText;
+            strCmdText = "/C dotnet VDI.Demo.Web.Host.dll";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
